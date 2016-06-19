94e85283bcc8503c42f6e93dbeac6d803b7bf8d3
Justin <justin.ison@gmail.com>
2016-06-19 19:03:43 +0200

    fixed file path

--- android/spec/setup.rb
+++ android/spec/setup.rb
@@ -50,6 +50,6 @@ end
 def attach_report_files example
   example.attach_file("Hub Log: #{ENV["UDID"]}", File.new("#{ENV["PROJECT_DIR"]}/output/hub.log")) unless ENV["THREADS"].nil?
   dir = Dir.entries("#{ENV["PROJECT_DIR"]}/output/").grep(/#{ENV["UDID"]}/)
-  files = dir.map { |file| { name: file.match(/(.*)-/)[1], file: file } }
+  files = dir.map { |file| { name: file.match(/(.*)-/)[1], file: "#{ENV["PROJECT_DIR"]}/output/#{file}" } }
   files.each { |file| example.attach_file(file[:name], File.new(file[:file])) } unless files.empty?
 end
