f473500ffe193f77835ffd49eae6d34ba44f0b4e
isonic1 <justin@Justins-MBP.fios-router.home>
2017-09-16 18:31:05 -0400

    small change

--- example/test-appium.rb
+++ example/test-appium.rb
@@ -1,10 +1,10 @@
 require 'appium_lib'
 
-caps = Appium.load_appium_txt file: File.join(File.dirname(__FILE__), 'appium.txt')
+caps = Appium.load_appium_txt 'appium.txt'
 caps[:appium_lib] = {:sauce_username=>false, :sauce_access_key=>false, :wait=>30}
-caps[:caps][:platformName] = "ANDROID"
-caps[:caps][:deviceName] = "android"
 caps[:caps][:app] = "#{Dir.pwd}/NotesList.apk"
+
+puts caps
 
 Appium::Driver.new(caps).start_driver
 Appium.promote_appium_methods Object
